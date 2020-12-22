a7abdff0fccb7b5cb3959bd9167b74aa0fcf53c2
nksh <shnapik@tut.by>
2020-12-22 10:13:54 +0300

    New page for page object with locators

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,13 +8,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        # url должен соответствовать ожидаемому
+        assert "login" in self.browser.current_url,   "word 'Login' not in link  or is not presented"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        # форма для логина должна присутствовать
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        # форма регистарации должна присутствовать
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not presented"
