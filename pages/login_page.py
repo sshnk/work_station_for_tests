9a607a6a3495642cf8e3206a04b122f21455a1a0
sshnk <72258731+sshnk@users.noreply.github.com>
2021-06-09 15:47:23 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,18 +9,15 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # url должен соответствовать ожидаемому
         assert "login" in self.browser.current_url,   "word 'Login' not in link  or is not presented"
 
     def should_be_login_form(self):
-        # форма для логина должна присутствовать
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # форма регистарации должна присутствовать
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not presented"
 
-    def register_new_user(self, email, password):  # создаём пользователя и заполняем нужные для регистрации поля
+    def register_new_user(self, email, password):  
         mail = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
         mail.send_keys(email)
         pass1 = self.browser.find_element(*LoginPageLocators.REGISTER_PASS1)
