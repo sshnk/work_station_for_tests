f4da08b35d484ad848d72212c7f942cc96be740b
sshnk <72258731+sshnk@users.noreply.github.com>
2021-06-09 15:46:10 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,3 @@
-# импортируем нужные нам пакеты, в данном случаи это конкретные классы отдельных страниц
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
@@ -8,9 +7,9 @@ class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser,
-                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+                        link)  
+        page.open()  
+        page.go_to_login_page()  
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
@@ -18,14 +17,14 @@ class TestLoginFromMainPage():
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
-        page.should_be_login_link()  # выполняем проверку что в url есть ожидаемый параметр
+        page.should_be_login_link() 
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_basketpage()  # идём на страницу с корзиной
+    page.go_to_basketpage()  
     basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_notification()  # делаем проверку на уведомление
-    basket_page.should_not_be_product_notification()  # делаем проверку на уведомление
+    basket_page.should_not_be_notification() 
+    basket_page.should_not_be_product_notification()  
